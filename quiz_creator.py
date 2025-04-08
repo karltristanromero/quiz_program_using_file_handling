2a479af4a1e5bab0c94bb7820ca02c7d4a15420c
Karl Romero <karltristanfromero@iskolarngbayan.pup.edu.ph>
2025-04-08 21:58:36 +0800

    Add initial code that will ask for the contents of questionnaire via user input

--- quiz_creator.py
+++ quiz_creator.py
@@ -3,9 +3,25 @@
 # to a text file. Ask another question until the user chose to exit.
 
 # Pseudocode
-# - def quiz_creator 
+# - def quiz_maker 
 # - append to file that contains the questionnaire
 # - while loop
 # - ask for input: question, answer options, correct answer
 # - append the user inputs to the file
 # - close file
+
+# Main Program
+
+def quiz_maker():
+    questionnaire = open("questionnaire.txt", "a")
+
+    while True:
+        question = input("Enter a question entry: ")
+
+        for i in range(4):
+            letter = chr(ord("a") + i)
+            possible_answer = input(f"Enter an answer for {letter}")
+
+        right_answer = input("Enter the correct letter of the answer: ")
+
+quiz_maker()

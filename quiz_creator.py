b2d2e0540fd30f05ed48db98645fc612016360c9
Karl Romero <karltristanfromero@iskolarngbayan.pup.edu.ph>
2025-04-08 23:41:41 +0800

    Transfer the pseudocode from the main program to the pseudocode file

--- quiz_creator.py
+++ quiz_creator.py
@@ -1,15 +1,3 @@
-# Create a program that ask user for a question, it will also ask for 4 
-# possible answer (a,b,c,d) and the correct answer. Write the collected data 
-# to a text file. Ask another question until the user chose to exit.
-
-# Pseudocode
-# - def quiz_maker 
-# - append to file that contains the questionnaire
-# - while loop
-# - ask for input: question, answer options, correct answer
-# - append the user inputs to the file
-# - close file
-
 # Main Program
 
 import os

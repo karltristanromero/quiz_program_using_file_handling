049f891573eba2f12b39f97409f1ec52090d37b5
Karl Romero <karltristanfromero@iskolarngbayan.pup.edu.ph>
2025-04-30 21:48:01 +0800

    Made a function that automatically finds the file

--- quiz_game.py
+++ quiz_game.py
@@ -1,1 +1,19 @@
+def file_finder(file_name):
+    import os
 
+    basedir = os.path.dirname(os.path.abspath(__file__))
+    categorization_file = os.path.join(basedir, file_name)
+
+    return categorization_file
+
+def start_quiz(file):
+
+    with open(file, "r") as questionnaire:
+        questions = questionnaire.readlines()
+        print(questions)
+
+        for question in questions:
+            parts = question.split()
+
+questionnaire = file_finder("science.txt")
+start_quiz(questionnaire)

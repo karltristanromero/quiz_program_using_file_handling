69be198fb56ad8385e6cc0c6116920a9a22830d2
Karl Romero <karltristanfromero@iskolarngbayan.pup.edu.ph>
2025-04-30 22:17:26 +0800

    Add initial code where program can now print and take inputs from the user

--- quiz_game.py
+++ quiz_game.py
@@ -9,11 +9,28 @@ def file_finder(file_name):
 def start_quiz(file):
 
     with open(file, "r") as questionnaire:
-        questions = questionnaire.readlines()
-        print(questions)
+        list_of_qna = questionnaire.readlines()
 
-        for question in questions:
-            parts = question.split()
+        score = 0
+        for qna in list_of_qna:
+            parts = qna.split(" | ")   
 
+            ask_questions(parts) 
+            guess = input("Enter the letter of your answer (a/b/c/d): ")
+            point = validate_answer(guess, parts[5])
+            score += point
+
+def ask_questions(qna):
+    print(qna[0])
+
+    for i in range(4):
+        print(f"{chr(97+i)}. {qna[1+i]}")
+
+def validate_answer(answer, correct_answer):
+    if answer == correct_answer:
+        return 1
+    else:
+        return 0
+    
 questionnaire = file_finder("science.txt")
 start_quiz(questionnaire)

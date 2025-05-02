a856fbf829802569da00418f81cb7a4ed8cd060b
Karl Romero <karltristanfromero@iskolarngbayan.pup.edu.ph>
2025-05-02 16:09:22 +0800

    Finished the code for the actual game where it also counts the number of points.

--- quiz_game.py
+++ quiz_game.py
@@ -12,25 +12,54 @@ def start_quiz(file):
         list_of_qna = questionnaire.readlines()
 
         score = 0
+        questionnaire = []
         for qna in list_of_qna:
             parts = qna.split(" | ")   
 
-            ask_questions(parts) 
-            guess = input("Enter the letter of your answer (a/b/c/d): ")
-            point = validate_answer(guess, parts[6])
-            score += point
+            display_questions(parts)
 
-def ask_questions(qna):
+            guess = input("Enter the letter of your answer (a/b/c/d): ")
+            point, correct_ans = validate_answer(guess, parts[6])
+
+            parts, score = score_keeper(parts, score, point, correct_ans)
+            questionnaire.append(parts)
+
+        print(f"Your score is: {score}")
+
+        for number, qna in enumerate(questionnaire):
+            display = f" {number+1}. {display_questions(qna)} {qna[7]}"
+            print(display)
+
+
+def display_questions(qna):
     print(qna[1])
 
     for i in range(4):
         print(f"{chr(97+i)}. {qna[2+i]}")
 
 def validate_answer(answer, correct_answer):
+    correct_answer = correct_answer.split()[0]
+
     if answer == correct_answer:
-        return 1
+        return 1, correct_answer
     else:
-        return 0
+        return 0, correct_answer
+
+def score_keeper(qna_list, total_score, value_pt, answer):
+    if value_pt == 1:
+        total_score += value_pt
+        qna_list.append(" | Correct✅!")
+    else:
+        answer = f" | Incorrect❌! It was {answer}!"
+        qna_list.append(answer)
+
+    return qna_list, total_score
     
+
+def display_answers():
+    pass
+
+
+
 questionnaire = file_finder("science.txt")
 start_quiz(questionnaire)

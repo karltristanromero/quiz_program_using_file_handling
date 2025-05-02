0a17a635e5ad0ccfb2b9d44ff208839d8bc8badd
Karl Romero <karltristanfromero@iskolarngbayan.pup.edu.ph>
2025-05-02 16:40:16 +0800

    refactor: Transferred the code lines from quiz game to quiz program

--- quiz_program.py
+++ quiz_program.py
@@ -66,6 +66,43 @@ def prompt_correct_answer():
 
         if correct_ans in ["a", "b", "c", "d"]:
             return correct_ans
+        
+def display_answers(qna_list):
+    response = prompt_validation("Do you want to see the answers? (y/n): ")
+
+    if response.lower() == "y":
+        for number, qna in enumerate(qna_list):
+            display = f"{number+1}. {display_questions(qna)}{qna[7]}\n"
+            print(display)    
+    elif response == "n":
+        continue_or_end()
+
+def display_questions(qna):
+    question_str = qna[1] + "\n"
+
+    for i in range(4):
+        letter = chr(ord("a") + i)
+        question_str += f"{letter}) {qna[i+2]}\n"
+
+    return question_str
+
+def validate_answer(answer, correct_answer):
+    correct_answer = correct_answer.split()[0]
+
+    if answer == correct_answer:
+        return 1, correct_answer
+    else:
+        return 0, correct_answer
+
+def score_keeper(qna_list, total_score, value_pt, answer):
+    if value_pt == 1:
+        total_score += value_pt
+        qna_list.append("Correct✅!")
+    else:
+        answer = f"Incorrect❌! It was {answer}!"
+        qna_list.append(answer)
+
+    return qna_list, total_score
 
 def continue_or_end():
     while True:
@@ -164,6 +201,28 @@ def quiz_maker():
             
             qna_index += 1
 
+def start_quiz(file):
+
+    with open(file, "r") as questionnaire:
+        list_of_qna = questionnaire.readlines()
+
+        score = 0
+        questionnaire = []
+        for qna in list_of_qna:
+            parts = qna.split(" | ")   
+
+            print(display_questions(parts))
+
+            guess = input("Enter the letter of your answer (a/b/c/d): ")
+            point, correct_ans = validate_answer(guess, parts[6])
+
+            parts, score = score_keeper(parts, score, point, correct_ans)
+            questionnaire.append(parts)
+
+        ascii_art(f"Your score is: {score}")
+        display_answers(questionnaire)
+
+
 def qna_deleter():
     
     clear_screen()
@@ -220,6 +279,6 @@ def qna_deleter():
         else:
             print("Index given does not exist.")
 
-    
+
 if __name__ == "__main__":
     manage_files()

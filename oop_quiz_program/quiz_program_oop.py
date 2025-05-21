dd3b9207578c6a33bf0a14cd1e5b293b6ae8c381
Karl Romero <karltristanfromero@iskolarngbayan.pup.edu.ph>
2025-05-21 19:22:39 +0800

    Created first class code for prompt_validation

--- oop_quiz_program/quiz_program_oop.py
+++ oop_quiz_program/quiz_program_oop.py
@@ -1,1 +1,25 @@
 
+class PromptValidator:
+    def __init__(self, prompt, valid_response=None):
+        self.prompt = prompt
+        self.valid_response = valid_response
+
+    def get_input(self):
+        while True:
+
+            response = input(self.prompt).strip().lower()
+
+            if not response: 
+                print("Your input is invalid!")
+                continue
+
+            if self.valid_response:
+                if response in self.valid_response:
+                    return response
+                print(f"Input is invalid! Enter only {','.join(self.valid_response)}!")
+            else:
+                return response
+            
+question_validator = PromptValidator("Enter a question entry: ")
+print(question_validator.get_input())
+

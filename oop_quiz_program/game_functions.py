144b97939aabdd7165b6872d6eae100afd0666f1
Karl Romero <karltristanfromero@iskolarngbayan.pup.edu.ph>
2025-05-22 01:54:21 +0800

    Transfer the AnswerValidator class for function classification

--- oop_quiz_program/game_functions.py
+++ oop_quiz_program/game_functions.py
@@ -1,1 +1,16 @@
+import prompt_functions
 
+class AnswerValidator:
+    '''This is a code for validate_answer()'''
+    
+    def __init__(self, answer, correct_answer):
+        self.answer = answer
+        self.correct_answer = correct_answer
+
+    def validate_ans(self):
+        self.correct_answer = self.correct_answer.split()[0]
+
+        if self.answer == self.correct_answer:
+            return 1, self.correct_answer
+        else:
+            return 0, self.correct_answer
--- oop_quiz_program/prompt_functions.py
+++ oop_quiz_program/prompt_functions.py
@@ -68,21 +68,6 @@ class CorrectAnswerEntry(PromptValidator):
         correct_ans = self.get_input()
         return correct_ans
 
-class AnswerValidator:
-    '''This is a code for validate_answer()'''
-    
-    def __init__(self, answer, correct_answer):
-        self.answer = answer
-        self.correct_answer = correct_answer
-
-    def validate_ans(self):
-        self.correct_answer = self.correct_answer.split()[0]
-
-        if self.answer == self.correct_answer:
-            return 1, self.correct_answer
-        else:
-            return 0, self.correct_answer
-
 class ContinueOrExit(PromptValidator):
     '''This is a class for continue_or_exit()'''
     

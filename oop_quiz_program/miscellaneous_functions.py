507fb673ebeebb481e1848a937fd92f66f289f9f
Karl Romero <karltristanfromero@iskolarngbayan.pup.edu.ph>
2025-05-22 02:20:38 +0800

    Convert the UI-modifier codes int OOP

--- oop_quiz_program/miscellaneous_functions.py
+++ oop_quiz_program/miscellaneous_functions.py
@@ -1,1 +1,20 @@
+import os
+import time
+from art import text2art
 
+class UICleaner:
+
+    @staticmethod
+    def clear_screen():
+        os.system('cls' if os.name == 'nt' else 'clear')
+
+    @staticmethod
+    def ascii_art(text):
+        print(text2art(text))
+
+    @staticmethod
+    def file_not_exists_warning():
+        UICleaner.clear_screen()
+        UICleaner.ascii_art("File does not exist.")
+        time.sleep(3)
+        UICleaner.clear_screen()

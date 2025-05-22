d8b5b1f23bcfd727d6a2d47df6197a078b49617a
Karl Romero <karltristanfromero@iskolarngbayan.pup.edu.ph>
2025-05-22 10:24:56 +0800

    Restore the old version to avoid unit testing logical errors

--- oop_quiz_program/prompt_functions.py
+++ oop_quiz_program/prompt_functions.py
@@ -92,20 +92,20 @@ class FileRetriever(PromptValidator):
 if __name__ == "__main__":
     # This will store all of the objects
 
-    # question_validator = QuestionEntry("Enter a question entry: ")
-    # choices_validator = ChoicesEntry()
-    # correct_answer_validator = CorrectAnswerEntry("Enter the letter of the correct answer: ", ["a", "b", "c", "d"])
+    question_validator = QuestionEntry("Enter a question entry: ")
+    choices_validator = ChoicesEntry()
+    correct_answer_validator = CorrectAnswerEntry("Enter the letter of the correct answer: ", ["a", "b", "c", "d"])
 
 
-    # decision = ContinueOrExit()
-    # print(decision.continue_or_exit())
+    decision = ContinueOrExit()
+    print(decision.continue_or_exit())
 
-    # # This will store the behavior of the object
-    # question = question_validator.get_question()
-    # print(question)
+    # This will store the behavior of the object
+    question = question_validator.get_question()
+    print(question)
 
-    # choices = choices_validator.get_choices()
-    # print(choices)
+    choices = choices_validator.get_choices()
+    print(choices)
 
-    # correct_answer = correct_answer_validator.get_correct_answer()
-    # print(correct_answer)
+    correct_answer = correct_answer_validator.get_correct_answer()
+    print(correct_answer)

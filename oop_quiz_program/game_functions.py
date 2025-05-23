bd98e8c3f776ba25416ef61ed898d26a0ac79d4b
Karl Romero <karltristanfromero@iskolarngbayan.pup.edu.ph>
2025-05-24 00:01:02 +0800

    Add the functions that will display the questions or answers of a txt file or an answered file

--- oop_quiz_program/game_functions.py
+++ oop_quiz_program/game_functions.py
@@ -1,6 +1,6 @@
 from file_handling import FileHandler
 from miscellaneous_functions import UICleaner
-from prompt_functions import ContinueOrExit
+from prompt_functions import ContinueOrExit, PromptValidator
 
 class AnswerValidator:
     '''This is a code for validate_answer()'''
@@ -9,7 +9,7 @@ class AnswerValidator:
         self.answer = answer
         self.correct_answer = correct_answer
 
-    def validate_ans(self):
+    def valid_ans(self):
         self.correct_answer = self.correct_answer.split()[0]
 
         if self.answer == self.correct_answer:
@@ -67,6 +67,46 @@ class ShowFileContents:
             except FileNotFoundError:
                 FileHandler.file_not_exists_warning()
                 return
+
+class DisplayQnA:
+
+    def __init__(self, qna):
+        UICleaner.clear_screen()
+        self.qna = qna
+
+    def display_questions(self):
+        question_str = self.qna[1] + "\n"
+
+        for i in range(4):
+            letter = chr(ord("a") + i)
+            question_str += f"{letter}. {self.qna[i+2]}\n"
+
+        return question_str
+
+class DisplayAnswers():
+    
+    def __init__(self, qna_list):
+        self.qna_list = qna_list
+    
+    def display_ans(self):
+        prompt_check = "Do you want to see the answers? (y/n): "
+        valid_response = ["y", "n"]
+
+        response = PromptValidator(prompt_check, valid_response).get_input()
+
+        if response == "y":
+            UICleaner.clear_screen()
+            UICleaner.ascii_art("Answers\n")
+
+            for number, qna in enumerate(self.qna_list):
+                questions = DisplayQnA(qna).display_questions()
+                display = f"{number+1}. {questions}{qna[7]}\n"
+                print(display)    
+                input("Press any key to continue...\n")
+            return
+
+        elif response == "n":
+            return
 
 if __name__ == "__main__":
     # This will store the objects
